ea866d0027a6f20bee982412a67b040c31de1bdb
Cesar Hernandez <cesar.lopex@gmail.com>
2022-06-10 08:51:56 -0600

    agregando lazy

--- src/lazy.js
+++ src/lazy.js
@@ -2,17 +2,22 @@ const isIntersecting = (entry) => {
     return entry.isIntersecting; //true dentro de la pantalla
 };
 
-const accion = (entry) => {
-    const nodo = entry.target;
-    console.log("holis");
+const loadImage = (entry) => {
+    const container = entry.target;
+    const imagen = container.firstChild;
+    const url = imagen.dataset.src;
+    //cargue la imagen
+    imagen.src = url;
+    
+    //console.log("holis");
     //desregistra la imagen
-    observer.unobserve(nodo);
+    observer.unobserve(container);
 };
 
 const observer = new IntersectionObserver((entries)=>{
  entries
   .filter(isIntersecting)
-  .forEach(accion);
+  .forEach(loadImage);
 
 });
 export const registerImage = (imagen) => {
