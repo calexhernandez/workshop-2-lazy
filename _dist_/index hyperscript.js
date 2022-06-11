68a99f202c056bf5f65346bd58b36f0e2c6bb540
calexhernandez <calexhernandez@users.noreply.github.com>
2022-06-11 04:20:46 +0000

    deploy: eb4fad9f1a0c4708acef4391f9e7b365b25ef519

---
+++ _dist_/index hyperscript.js
@@ -0,0 +1,45 @@
+/**
+ * This file is just a silly example to show everything working in the browser.
+ * When you're ready to start on your site, clear the file. Happy hacking!
+ **/
+import h  from '../_snowpack/pkg/hyperscript.js'
+import {registerImage} from './lazy.js'
+console.log('Happy hacking :)')
+
+const minimun = 1
+const maximun = 122
+
+const random = () => Math.floor(Math.random() * (maximun - minimun)) + minimun
+
+const createImageNode = () => {
+
+/*    const imagen = document.createElement('img');
+    imagen.className = 'mx-auto';
+    imagen.width = '320';
+    imagen.dataset.src =  `https://randomfox.ca/images/${random()}.jpg`;
+*/
+    const imagen = h("img.mx-auto",{
+      width: "320",
+      "data-src": `https://randomfox.ca/images/${random()}.jpg`,
+    });
+
+    //const container = document.createElement("div");
+    //container.className = "p-4";
+    const container = h('div.p-4', imagen);
+
+//    container.appendChild(imagen);
+
+    return container;
+}
+
+const nuevaImagen = createImageNode();
+const mountNode = document.getElementById('images');
+
+const addButton = document.querySelector("button");
+const addImage = () => {
+  const newImage = createImageNode();
+  mountNode.append(newImage);
+  registerImage(newImage);
+}
+
+addButton.addEventListener("click", addImage)
